57371fc2667885bcec04b98bef9b8c99f5bf3cf2
JotaPePG <obmitimbo@gmail.com>
2024-05-16 08:46:36 -0300

    some styles

--- assets/mais.js
+++ assets/mais.js
@@ -25,25 +25,17 @@ const tabClicked = (tab) => {
   svg[0].setAttribute("class", "svg selected");
 };
 
-// Função para salvar tarefas no localStorage
 function salvarTarefa(task) {
-  // Verificar se já existem tarefas salvas
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  // Adicionar a nova tarefa ao array de tarefas
   tasks.push(task);
 
-  // Salvar o array de tarefas atualizado no localStorage
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// Obter o formulário e o botão de envio
-
-// Adicionar um ouvinte de evento para o envio do formulário
 formElement.addEventListener("submit", function (event) {
   event.preventDefault(); // Impedir o envio do formulário
 
-  // Obter os valores dos inputs
   const taskDescription = document.querySelector(".inserttask").value;
   const taskTime = document.querySelector(".inserttime").value;
   const taskDate = document.querySelector(".insertdate").value;
@@ -51,7 +43,6 @@ formElement.addEventListener("submit", function (event) {
 
   tabelaContainer.innerHTML = "";
 
-  // Criar um objeto para representar a nova tarefa
   const newTask = {
     description: taskDescription,
     time: taskTime,
@@ -60,23 +51,15 @@ formElement.addEventListener("submit", function (event) {
 
   listaDeTarefas.push(newTask);
 
-  // Salvar a nova tarefa no localStorage
   salvarTarefa(newTask);
-  // Limpar os inputs após salvar a tarefa
   formElement.reset();
-
-  // const tabelaMinhaTarefa = criarTabelaParaTarefa(newTask);
-  // tabelaContainer.appendChild(tabelaMinhaTarefa);
 
   console.log(newTask);
   console.log(listaDeTarefas);
 
-  // Verificar se existem tarefas salvas no localStorage
   const tasksJSON = localStorage.getItem("tasks");
 
-  // Verificar se há tarefas salvas no localStorage
   if (tasksJSON) {
-    // Converter os dados JSON de volta para objetos JavaScript
     const tasks = JSON.parse(tasksJSON);
     let listaDeHorarios = [];
     tasks.forEach(function (task) {
@@ -86,7 +69,6 @@ formElement.addEventListener("submit", function (event) {
       };
       listaDeHorarios.push(objeto);
     });
-    // Iterar sobre cada tarefa e criar uma tabela para ela
     console.log(listaDeHorarios);
     listaDeHorarios = listaDeHorarios.sort((a, b) => {
       const timeA = a.time.split(":").map(Number);
@@ -100,7 +82,6 @@ formElement.addEventListener("submit", function (event) {
       return dateA - dateB;
     });
     listaDeHorarios.forEach(function (task) {
-      // Criar e adicionar uma tabela para a tarefa atual
       const tabelaTarefa = criarTabelaParaTarefa(task.tarefa);
       const taskDay = task.tarefa.date.slice(-2);
       console.log(taskDay);
@@ -109,32 +90,26 @@ formElement.addEventListener("submit", function (event) {
       }
     });
   } else {
-    // Se não houver tarefas salvas, exibir uma mensagem ou realizar alguma outra ação
     console.log("Não há tarefas armazenadas no localStorage.");
   }
 });
 
 function buscarTarefas() {
-  // Verificar se existem tarefas salvas
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   return tasks;
 }
 
 function buscarElementoDeTarefa(elemento, indice) {
-  // Buscar todas as tarefas
   const tasks = buscarTarefas();
 
-  // Verificar se o índice fornecido é válido
   if (indice >= 0 && indice < tasks.length) {
-    // Retornar o elemento específico da tarefa no índice dado
     return tasks[indice][elemento];
   } else {
-    return null; // Retornar null se o índice for inválido
+    return null;
   }
 }
 
 function criarTabelaParaTarefa(task) {
-  // Criar os elementos HTML para a tabela da tarefa
   const taskDiv = document.createElement("div");
   taskDiv.classList.add("task");
 
@@ -154,26 +129,20 @@ function criarTabelaParaTarefa(task) {
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("class", "checkbox-todaytasks");
 
-  // Adicionar os elementos criados à tabela da tarefa
   timeCheckboxDiv.appendChild(taskTime);
   timeCheckboxDiv.appendChild(checkbox);
   taskDiv.appendChild(taskName);
   taskDiv.appendChild(timeCheckboxDiv);
 
-  // Retornar a tabela da tarefa criada
   return taskDiv;
 }
 
 function criarTabelasParaTarefas(listaDeTarefas) {
-  // Selecionar o contêiner onde as tabelas serão inseridas
   const tabelaContainer = document.getElementById("taskdiv");
 
-  // Limpar qualquer conteúdo existente no contêiner
   tabelaContainer.innerHTML = "";
 
-  // Iterar sobre cada objeto de tarefa na lista
   listaDeTarefas.forEach(function (task) {
-    // Criar e adicionar uma tabela para o objeto de tarefa atual
     const tabelaTarefa = criarTabelaParaTarefa(task);
     tabelaContainer.appendChild(tabelaTarefa);
   });
